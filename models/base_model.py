e6f3dc7547d583b65de47ebe66bbcfc90cc1d0d8
Andrecast <acastrillonpuerta@gmail.com>
2021-08-12 01:16:01 +0700

    Modifying base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -22,12 +22,21 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at":
+                    setattr(self, key, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        value, '%Y-%m-%dT%H:%M:%S.%f'))
+                else:
+                    setattr(self, key, value)
+            if not "id" in self.__dict__:
+                self.id = str(uuid.uuid4())
+                self.created_at = datetime.now()
+                self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -44,15 +53,19 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        if "_sa_instance_state" in dictionary.keys():
-            del dictionary["_sa_instance_state"]
-        return dictionary
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key == 'created_at':
+                new_dict[key] = self.created_at.isoformat()
+            elif key == 'updated_at':
+                new_dict[key] = self.updated_at.isoformat()
+            elif key == '_sa_instance_state':
+                continue
+            else:
+                new_dict[key] = value
+
+        new_dict["__class__"] = self.__class__.__name__
+        return new_dict
 
     def delete(self):
         """Delete the current instance from the storage"""
