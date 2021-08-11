4b9591f769b724e8831c3b21b7ac7df1c0fb6827
Andrecast <acastrillonpuerta@gmail.com>
2021-08-12 05:12:20 +0700

    Fixing storage

--- console.py
+++ console.py
@@ -153,7 +153,7 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, key, value)
 
         print(new_instance.id)
-        storage.save()
+        new_instance.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -235,11 +235,11 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all(args).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage._FileStorage__objects.items():
+            for k, v in storage.all().items():
                 print_list.append(str(v))
 
         print(print_list)
