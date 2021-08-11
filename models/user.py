717325287f1c347f0854c70c0d6cd98358ede216
felipevargas-bz <felipe15angel@gmail.com>
2021-08-11 22:39:37 +0000

    modify user.py and city.py chamges: relationships

--- models/user.py
+++ models/user.py
@@ -15,4 +15,3 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
     places = relationship("Place", backref="user", cascade="all, delete")
-    reviews = relationship("Review", backref="user", cascade="all, delete")
