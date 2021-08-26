2abc802fa57d2b3254e44bbb3ca321cc6e52f9c4
Andrecast <acastrillonpuerta@gmail.com>
2021-08-27 02:11:21 +0700

    Fixing permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,9 +9,6 @@ fi
 # Create the folders
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-# Give ownership of the /data/ folder to the ubuntu user AND group 
-sudo chown -R ubuntu:ubuntu /data
-sudo chmod -R 755 /data/
 # Create a fake HTML file
 touch /data/web_static/releases/test/index.html
 echo "<html>
@@ -23,6 +20,9 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Give ownership of the /data/ folder to the ubuntu user AND group 
+sudo chown -R ubuntu:ubuntu /data
+sudo chmod -R 755 /data/
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 sudo sed -i '48 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 # Restart nginx
